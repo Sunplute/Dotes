21b628783bc2de8487dfb1a5eea32d72b55700fd
Splute Sun <sun_05202022@126.com>
2023-07-08 16:50:52 +0100

    1 ver

---
+++ app/src/main/java/com/example/dotes/NoteAdapter.java
@@ -0,0 +1,47 @@
+package com.example.dotes;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+
+public class NoteAdapter extends FirestoreRecyclerAdapter<Note, NoteAdapter.NoteViewHodler> {
+    Context context;
+
+    public NoteAdapter(@NonNull FirestoreRecyclerOptions<Note> options, Context context) {
+        super(options);
+        this.context = context;
+    }
+
+    @Override
+    protected void onBindViewHolder(@NonNull NoteViewHodler holder, int position, @NonNull Note note) {
+        holder.titleTextView.setText(note.title);
+        holder.contentTextView.setText(note.content);
+        holder.timestampTextView.setText(Utility.timestampToString(note.timestamp));
+    }
+
+    @NonNull
+    @Override
+    public NoteViewHodler onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.recycler_note_item, parent, false);
+        return new NoteViewHodler(view);
+    }
+
+    class NoteViewHodler extends RecyclerView.ViewHolder{
+        TextView titleTextView, contentTextView, timestampTextView;
+
+        public NoteViewHodler(@NonNull View itemView) {
+            super(itemView);
+            titleTextView = itemView.findViewById(R.id.note_title_text_view);
+            contentTextView = itemView.findViewById(R.id.note_content_text_view);
+            timestampTextView = itemView.findViewById(R.id.note_timestamp_text_view);
+        }
+    }
+}
